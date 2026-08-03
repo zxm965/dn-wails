e3bc2ee568f135baa4e82014a544467cff65acfb
zxm965 <zxm965@qq.com>
2026-08-03 14:37:18 +0800

    fix: support CRLF in release version config

--- scripts/configure-release.mjs
+++ scripts/configure-release.mjs
@@ -9,7 +9,7 @@ if (!/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$/.test(version)) {
 
 const configPath = new URL('../build/config.yml', import.meta.url)
 const config = await readFile(configPath, 'utf8')
-const versionPattern = /(^info:\n(?:^[ \t]+.*\n)*?^[ \t]+version:[ \t]*)(['"]?)[^'"\r\n]+\2([ \t]*(?:#.*)?$)/m
+const versionPattern = /(^info:\r?\n(?:^[ \t]+.*\r?\n)*?^[ \t]+version:[ \t]*)(['"]?)[^'"\r\n]+\2([ \t]*(?:#.*)?$)/m
 
 if (!versionPattern.test(config)) {
   throw new Error('build/config.yml is missing info.version')
